f02f6f1c54057361b4d57138020ed1eb018d1435
Estarossa27 <fatm-i@hotmail.com>
2022-03-17 16:54:05 +0100

    Vegeta3

---
+++ public/javascript/main.js
@@ -0,0 +1,16 @@
+let race = prompt('A quelle race appartenez-vous ?')
+let age = +prompt('Quelle âge avez vous ?')
+// let p = document.querySelector(p)
+
+if (age <= 26 ) {
+    alert('Vous êtes un ' + race + ' et vous avez '+ age )
+    p.style.color= "red" 
+}
+else (age >= 26 ); {
+    alert('vous êtes un ' + race + ' et vous avez '+ age)
+    p.style.color= "green"
+}
+/*
+p addEventListener('click',()) =>{
+    p.style.fontSize="100px"
+}*/
